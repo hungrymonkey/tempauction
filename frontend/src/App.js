ae93b0eaf7cf88b1bbaa39f1e8603aef75d78b4d
hungrymonkey <771339+hungrymonkey@users.noreply.github.com>
2021-05-01 15:59:00 -0700

    Routes (#18)
    Tab Page changes works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Link as RouterLink, Switch, Route, useHistory } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-import Header from './component/Header';
+import { fetchAllAuctions } from './handlers/get/fetchallauctions.js';
 import { Auction } from './Auction';
 
-import { fetchAllAuctions } from './handlers/get/fetchallauctions.js';
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-function App() {
-  const [auctionList, setAuctionList] = useState([]);
-  const [auctionIndex, setAuctionIndex] = useState(0);
-  useEffect(() => {
-    fetchAllAuctions().then(
-        (value) => setAuctionList(value)
-    )
-    // code to run on component mount
-  }, [])
-  var render = function(props) {
-    console.log(auctionList)
-    return (
-      <div className="App">
-        <Router >
-            <Switch>
-              <Route path="/auction/:id" child={<Auction auction={auctionList}/>}/>
-            </Switch>
-        </Router>
-        <Header auctions={auctionList}/>
-        <header className="App-header">
+
+function Home() {
+  return (
+    <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -42,10 +27,88 @@ function App() {
             Learn React
           </a>
         </header>
+  );
+}
+
+const headerStyles = { //Footer CSS
+	width: 'auto',
+	height: '60px',
+	padding: '0px',
+	backgroundColor: 'ghostwhite',
+};
+function a11yProps(index) {
+	return {
+		id: `simple-tab-${index}`,
+		'aria-controls': `simple-tabpanel-${index}`,
+	};
+}
+function a11yDummyProps(index) {
+	return {
+		id: `simple-dummy-tab-${index}`,
+		'aria-controls': `simple-dummy-tabpanel-${index}`,
+	};
+}
+
+
+function App() {
+  const [auctionList, setAuctionList] = useState([]);
+  const [auctionIndex, setAuctionIndex] = useState(0);
+
+  useEffect(() => {
+    fetchAllAuctions().then(
+        (value) => setAuctionList(value)
+    )
+    // code to run on component mount
+  }, [])
+
+  const handleIndexChange = (event, index) => {
+    console.log(index);
+    console.log(event)
+    setAuctionIndex(index);
+  }
+  var renderTabs = function() {
+    return (
+      auctionList.map((a, i) => (
+          <Tab key={"tab-"+(i+1)} label={a["data"]["name"]} {...a11yProps(i + 1)} component={RouterLink}  to={"/auction/"+a["data"]["name"]}/>
+        ))
+    );
+  }
+  var render = function(props) {
+    console.log(auctionList)
+    let hasAuctions = auctionList.length > 0;
+    return (
+      <div className="App" >
+        <Router>
+          <div>
+            <header style={headerStyles}>
+              <AppBar position="static" color="transparent">
+               <Tabs value={auctionIndex} aria-label="simple tabs example"  onChange={handleIndexChange}> 
+                  <Tab key={"tab-0"} label="Home" {...a11yProps(0)} component={RouterLink}  to="/" />
+                  {
+                    hasAuctions ? 
+                      renderTabs()
+                    : <Tab key={"tab-dummy"} label="No Auctions Found" {...a11yDummyProps(1)} />
+                  }
+                </Tabs>
+              </AppBar>       
+            </header> 
+            <Switch>
+              <Route exact path='/'>
+                <Home/>
+              </Route>
+              <Route path='/auction/:id'>
+                  <Auction auctions={auctionList}/>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+       
+        
       </div>
     );
   }
   return render({auctions: auctionList})
 }
+
 
 export default App;
--- frontend/src/Auction.jsx
+++ frontend/src/Auction.jsx
@@ -2,24 +2,33 @@
 import React, { useEffect, useState } from "react";
 import Header from './component/Header';
 
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch
+} from "react-router-dom";
 
 export function Auction(props) {
+
   useEffect(() => {
     // code to run on component mount
+    console.log("*********mounted Auction")
+    console.log(props)
   }, [])
   var render = function() {
     console.log(props.auctions)
+    console.log("******** rendering")
     return (
     <div className="Auction">
-      <Header auctions={[]}/>
-      <header className="App-header">
+      <header className="App-header2">
       
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
       <a
-        className="App-link"
+        className="App-link2"
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
