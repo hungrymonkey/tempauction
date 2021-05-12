7ed4bd4b3981f6ab26f77c023a006a6319accb14
hungrymonkey <771339+hungrymonkey@users.noreply.github.com>
2021-05-11 17:34:49 -0700

    https://www.aaae.ngo/ (#36)
    Added aaae.ngo to the whitelist

--- cloudflare/tempauction/src/config.js
+++ cloudflare/tempauction/src/config.js
@@ -1,2 +1,2 @@
 
-export const VALID_URL_SET = new Set(["http://localhost:3000", "https://frontend.tedchang2010.workers.dev"]);
+export const VALID_URL_SET = new Set(["http://localhost:3000", "https://frontend.tedchang2010.workers.dev", "https://www.aaae.ngo"]);
